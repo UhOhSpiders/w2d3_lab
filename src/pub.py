d3f0ec71d02d3c285aed0713c53e1f05cb338919
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-24 15:38:58 +0100

    extenstion almost done

--- src/pub.py
+++ src/pub.py
@@ -1,8 +1,10 @@
 class Pub():
-    def __init__(self, name, till):
+    def __init__(self, name, till, stock):
         self.name = name
         self.till = till
+        self.stock = stock
         self.drinks = []
+        
 
     def increase_till(self, new_sale_value):
         self.till += new_sale_value
@@ -10,7 +12,13 @@ class Pub():
     # def get_till(self):
     #     return self.till
 
-   
+    def stock_value(self):
+        value = 0
+        for ammount in self.stock:
+            
+            value += 
+        
+        return self.stock
     
     def add_stock(self, drinks):
         for drink in drinks:
@@ -19,6 +27,8 @@ class Pub():
     def check_drunkenness(self, customer):
         if customer.drunkenness < 60:
             return True
+        else:
+            return False
     
     def check_age(self, customer):
         if customer.age >= 18:
@@ -31,6 +41,12 @@ class Pub():
         if self.check_drunkenness(customer) and self.check_age(customer):
             self.increase_till(drink.price)
             customer.spend(drink.price)
+
+    def sell_food(self, customer, food):
+            self.increase_till(food.price)
+            customer.spend(food.price)
+            customer.drunkenness -= food.rejuvenation_level
+   
    
    
    
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -2,15 +2,21 @@ import unittest
 from src.pub import Pub
 from src.customer import Customer
 from src.drink import Drink
+from src.food import Food
 
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.guiness = Drink("Guiness", 4.50, 30)
         self.shandy = Drink("Shandy", 3.50, 20)
         self.drinks = [self.guiness, self.shandy]
-        self.pub = Pub("The Thirsty Badger", 100.00)
+        
         self.deborah = Customer("Deborah", 35.00, 57, 50)
         self.tiny_tim = Customer("Tiny Tim", 5.00, 8, 40)
+        self.apple_pie = Food("Apple Pie", 5.00, 30)
+        self.stovies = Food("Stovies", 4.00, 45)
+        self.stock = [{ "name" : "Guiness", "ammount" : 500}, 
+                      { "name" : "Shandy", "ammount" : 1000}]
+        self.pub = Pub("The Thirsty Badger", 100.00, self.stock)
         # self.pub.add_stock(self.guiness)
         # self.pub.add_stock(self.shandy)
 
@@ -21,6 +27,9 @@ class TestPub(unittest.TestCase):
         self.drinks = [self.guiness, self.shandy]
         self.pub.add_stock(self.drinks)
         self.assertEqual(2, len(self.pub.drinks))
+    
+    def test_check_stock(self):
+        self.assertEqual("Guiness", self.pub.stock[0]["name"])
 
     # def test_pub_has_till_value(self):
     #     self.assertEqual(100.00, self.pub.till)
@@ -35,11 +44,27 @@ class TestPub(unittest.TestCase):
         self.pub.sell_drink(self.customer, self.guiness)
         self.assertEqual(30.50, self.customer.wallet)
 
-    def test_check_age(self):
-        # self.customer = self.tiny_tim
+    def test_check_age_underage(self):
         result = self.pub.check_age(self.tiny_tim)
         self.assertEqual(False, result)
+
+    def test_check_age_of_age(self):
+        result = self.pub.check_age(self.deborah)
+        self.assertEqual(True, result)
+
+    def test_check_drunk(self):
+        self.deborah.drunkenness = 70
+        result = self.pub.check_drunkenness(self.deborah)
+        self.assertEqual(False, result)
        
+    def test_check_sober(self):
+        self.deborah.drunkenness = 59
+        result = self.pub.check_drunkenness(self.deborah)
+        self.assertEqual(True, result)
+    
+    
+    
+    
     def test_check_sale_tiny_tim(self):
         self.pub.sell_drink(self.tiny_tim, self.guiness)
         self.assertEqual(5.00, self.tiny_tim.wallet)
@@ -52,7 +77,17 @@ class TestPub(unittest.TestCase):
         self.deborah.drunkenness = 70
         self.pub.sell_drink(self.deborah, self.guiness)
         self.assertEqual(35, self.deborah.wallet)
+
+    def test_sell_food(self):
+        self.customer = self.deborah
+        self.pub.sell_food(self.customer, self.stovies)
+        self.assertEqual(31, self.customer.wallet)
     
+    def test_rejuvenation(self):
+        self.customer = self.deborah
+        self.pub.sell_food(self.customer, self.stovies)
+        self.assertEqual(5, self.customer.drunkenness)
+
     
     # def test_get_stock_count(self):
     #     self.get_stock_count()
